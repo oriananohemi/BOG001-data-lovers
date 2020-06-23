040bb1f48595eee5eb624310ffaefed93a2f43f1
Sthephany Granados <lsgranados04@gmail.com>
2020-06-23 12:53:22 -0500

    Se adiciona codigo para paginacion

--- src/main.js
+++ src/main.js
@@ -35,3 +35,56 @@ data.pokemon.map((pokemon) => {
 })
 
 console.log(data);
+
+//paginacion
+
+let pok = [];
+
+
+for (let i = 0; i < pokemon.length; i++) {
+    pok.push(pokemon[i].name, `<img src= "${pokemon[i].img}"`);
+}
+
+//console.log(pok)
+document.getElementById('pokemones').innerHTML = pok;
+
+
+//Los valores de tipo pokemon estan en ingles
+
+let pageNumber = 1;         //En que pagina se inicia....en la primera                  
+let pageSize = 12;          //Cantidad de elementos que se van a mostrar por pagina     
+let pokemonesHtml = [];       //array que muestra el html de los pokemones  
+let pagination;             //elementos que se van a mostrar en la pagina actual    
+let pageCont = Math.ceil(pok.length / pageSize); //contenido de la pagina, se utiliza ceil para aproximar hacia arriba 
+
+function paginate(array, page_size, page_number) {      
+    return array.slice((page_number - 1) * page_size, page_number * page_size);  //intervalo del array
+}
+
+function nextPage(){
+    pageNumber ++;
+    showPokemons(pagination)
+}
+
+function previusPage(){
+    pageNumber --;
+    showPokemons(pagination)
+}
+
+function showPokemons(){
+    let pagination = paginate (pok,pageSize,pageNumber); //array, elementos por pag, pagina en que inicia 
+    //console.log("nextPage",pagination)
+    pokemonesHtml = "<ul>";
+    pagination.forEach(function(element){   //agregar cada elemento a una lista html
+        console.log(element);
+        pokemonesHtml += "<li>"+ element +"</li>";  
+    })
+    pokemonesHtml += "</ul>";
+    pokemonesHtml += pageNumber > 1  ? "<button onclick='previusPage()'>Anterior</button>":"";
+    pokemonesHtml += pageNumber < pageCont ? " <button onclick='nextPage()'>Siguiente</button>":"" ;
+    document.getElementById("list_pokemons").innerHTML = "";
+    document.getElementById("list_pokemons").innerHTML = pokemonesHtml;
+    //console.log(pokemonesHtml);
+}
+
+showPokemons(pagination);
